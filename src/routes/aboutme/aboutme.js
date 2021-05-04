0e1b6c20ce99ffd39e4c842da903539955893f85
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-05-04 18:54:37 -0400

    added profile picture to adout me page

--- src/routes/aboutme/aboutme.js
+++ src/routes/aboutme/aboutme.js
@@ -1,12 +1,37 @@
-import './aboutme.css';
-import background from '../../assets/images/aboutme.jpg';
+import "./aboutme.css";
+import background from "../../assets/images/aboutme.jpg";
+import profilepicture from "../../assets/images/profile.jpg";
 
-function AboutMe(){
-    return (
-        <div className="page" id="aboutme" style = {{backgroundImage: `url(${background})`}}>
-
-        </div>
-    );
+function AboutMe() {
+  return (
+    <div
+      className="page"
+      id="aboutme"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div id="profile-picture-main-container">
+        <div
+          id="profile-picture-sub-container"
+          style={{
+            backgroundColor: "#fffffe",
+            height: "200px",
+            width: "200px",
+            borderRadius: "100px",
+            margin: "auto",
+          }}
+        ></div>
+        <div
+          id="profile-picture"
+          style={{
+            backgroundImage: `url(${profilepicture})`,
+            height: "180px",
+            width: "180px",
+            borderRadius: "100px",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
