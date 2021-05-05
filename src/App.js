c346e5758260dbc18fef8d822224b0d2b52ed031
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-05-05 12:10:16 -0400

    file format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
 
-import NavigationBar from './widget/navigationbar/navigationbar';
-import AboutMe from './routes/aboutme/aboutme.js';
+import NavigationBar from "./widget/navigationbar/navigationbar";
+import AboutMe from "./routes/aboutme/aboutme.js";
 
 function App() {
-  return (<div>
-    <NavigationBar/>
-    <AboutMe/> 
-  </div>);
+  return (
+    <div>
+      <NavigationBar />
+      <AboutMe />
+    </div>
+  );
 }
 
 export default App;
