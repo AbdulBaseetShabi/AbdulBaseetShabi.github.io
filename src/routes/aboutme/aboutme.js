0e05cd48694ee00226aa3db782e54ea9adee9053
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-05-05 12:09:53 -0400

    using animated console

--- src/routes/aboutme/aboutme.js
+++ src/routes/aboutme/aboutme.js
@@ -1,7 +1,8 @@
 import "./aboutme.css";
 import background from "../../assets/images/aboutme.jpg";
 import profilepicture from "../../assets/images/profile.jpg";
-import AnimatedText from "../../widget/animatedtext/animatedtext";
+import CustomButton from "../../widget/button/custombutton";
+import AnimatedConsole from "../../widget/animatedconsole/animatedconsole";
 
 function AboutMe() {
   return (
@@ -31,15 +32,47 @@ function AboutMe() {
           }}
         ></div>
       </div>
-      <AnimatedText
-        prefixText="> "
-        suffixText="|"
-        texts={["What$is$my$name$", "life$is$fair", "what$to do"]}
-        textChangeInterval={2100}
-        typingSpeed={200}
-        generalStyle={{ fontWeight: "bold", backgroundColor: "transparent", width: "100%", fontSize: "5rem", textAlign: "center", marginTop: "6rem"}}
-        specialCharaterStyle={{ color: "#BB6BD9" }}
-      />
+      <div style={{marginTop: "4rem"}}>
+        <AnimatedConsole
+          prefixText="> "
+          suffixText="|"
+          texts={["Hi, my name is$Abdul-Baseet", "Welcome to my$website$"]}
+          textChangeInterval={1200}
+          typingSpeed={50}
+          generalStyle={{
+            fontWeight: "bold",
+            backgroundColor: "transparent",
+            width: "100%",
+            fontSize: "5rem",
+            textAlign: "center",
+            paddingTop: "0.5rem",
+            opacity: "95%"
+          }}
+          specialCharaterStyle={{ color: "#BB6BD9" }}
+        />
+      </div>
+
+      <div style={{ textAlign: "center", marginTop: "6rem" }}>
+        <CustomButton
+          backgroundColor="#272C45"
+          opacity="80%"
+          text="Download Resume"
+        />
+      </div>
+      <div style={{ width: "100%", position: "absolute", bottom: "0" }}>
+        <svg
+          width="100%"
+          viewBox="0 0 1440 121"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 0L80 17.6301C160 35.4492 320 70.4258 480 68.0625C640 65.6992 800 25.0508 960 17.6301C1120 9.92578 1280 35.4492 1360 47.8801L1440 60.5V121H1360C1280 121 1120 121 960 121C800 121 640 121 480 121C320 121 160 121 80 121H0V0Z"
+            fill="#0F0E17"
+            fillOpacity="0.95"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
