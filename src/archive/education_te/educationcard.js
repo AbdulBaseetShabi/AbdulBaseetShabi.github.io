437fcea702231584f347ab1f31a3709bd1e77c3d
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-04 11:23:07 -0400

    archived code

---
+++ src/archive/education_te/educationcard.js
@@ -0,0 +1,33 @@
+function EducationCard(props) {
+  return (
+    <div
+      style={{
+        margin: props.position % 2 === 0 ? "0 1rem 0 0" : "0 0 0 1rem",
+        backgroundColor: props.backgroundColor
+      }}
+      className="education-card"
+      position={props.position}
+    >
+      <label className="school-label">
+        {props.school.toUpperCase()}
+      </label>
+      <br />
+      <label
+        className="inline-block-label"
+        style={{
+          fontSize: "1.5rem",
+          margin: "1rem auto",
+        }}
+      >
+        {props.program.toUpperCase()}
+      </label>
+      <br />
+      <label
+        className="inline-block-label"
+        style={{ fontSize: "1.1rem" }}
+      >{`${props.from.toUpperCase()} - ${props.to.toUpperCase()}`}</label>
+    </div>
+  );
+}
+
+export default EducationCard;
