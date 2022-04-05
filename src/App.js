b36d4900f887aed5753735710bdb3c6ee36efc02
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-05 10:25:39 -0400

    Archive - Thank you and Horizontal Line

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import AboutMe from "./routes/aboutme/aboutme.js";
 import Education from "./routes/education/education";
 import Experiences from "./routes/experiences/experiences";
 import Projects from "./routes/projects/projects";
-import ThankYou from "./routes/thankyou/thankyou";
 
 function App (props) { 
     return (
