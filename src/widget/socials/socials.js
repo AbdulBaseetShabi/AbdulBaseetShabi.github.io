ce7922ffe675b6439f95031792c0d1119ef17367
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-19 18:03:01 -0400

    changed icons, moved css to file

--- src/widget/socials/socials.js
+++ src/widget/socials/socials.js
@@ -6,26 +6,26 @@ const SOCIALS = [
   {
     name: "Resume",
     url: "https://docs.google.com/viewer?url=https://github.com/AbdulBaseetShabi/web-resume/raw/master/public/assets/resume/resume.pdf",
-    src: "https://img.icons8.com/plasticine/90/000000/file.png",
+    src: "https://img.icons8.com/plasticine/60/000000/file.png",
   },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/abdulbaseet/",
-    src: "https://img.icons8.com/bubbles/100/000000/linkedin.png",
+    src: "https://img.icons8.com/fluency/60/000000/linkedin.png",
   },
   {
     name: "Instagram",
     url: "https://www.instagram.com/bsquare480/",
-    src: "https://img.icons8.com/clouds/100/000000/instagram-new--v2.png",
+    src: "https://img.icons8.com/fluency/60/000000/instagram-new.png",
   },
   {
     name: "Email",
     url: "mailto:shab7430@mylaurier.ca",
-    src: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/100/000000/external-email-digital-nomad-flaticons-lineal-color-flat-icons.png",
+    src: "https://img.icons8.com/fluency/60/000000/email-open.png",
   },
   {
     name: "GitHub",
-    src: "https://img.icons8.com/cute-clipart/75/000000/github.png",
+    src: "https://img.icons8.com/glyph-neue/70/000000/github.png",
     url: "https://github.com/AbdulBaseetShabi",
   },
 
@@ -48,37 +48,8 @@ class Socials extends React.Component {
   }
 
   render() {
-    let style_container =
-      this.props.orientation === "horizontal"
-        ? {}
-        : {
-            display: "flex",
-            alignItems: "center",
-            margin: "1.5rem auto",
-            width: "50vw",
-            justifyContent: "center",
-          };
-    let style_tooltip =
-      this.props.orientation === "horizontal"
-        ? {
-            marginLeft: "10px",
-            backgroundColor: "#272c45",
-            // opacity: 0.8,
-            color: "white",
-          }
-        : {
-            // backgroundColor: "#272c45",
-            backgroundColor: "black",
-            // opacity: 0.8,
-            color: "white",
-            display: "block",
-            textAlign: "center",
-            position: "relative",
-            bottom: "0",
-            fontStyle: "italic",
-          };
     return (
-      <div id="socials-container" style={style_container}>
+      <div id="socials-container">
         {SOCIALS.map((social, index) => {
           return (
             <div className="social" key={index} style={{width: `${100/SOCIALS.length}%`}}>
@@ -92,7 +63,7 @@ class Socials extends React.Component {
                   GlobalFunction.openLinkInNewTab(social.url);
                 }}
               ></img>
-              <label className="tooltip" style={style_tooltip}>
+              <label className="tooltip">
                 {social.name}
               </label>
             </div>
