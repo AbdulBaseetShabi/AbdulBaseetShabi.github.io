515ade71554528fbe8100213d6cd855bad180fe1
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-20 07:23:33 -0400

    single page application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import NavigationBar from "./widget/navigationbar/navigationbar";
@@ -8,19 +7,16 @@ import Education from "./routes/education/education";
 import Experience from "./routes/experience/experience";
 import Projects from "./routes/projects/projects";
 
-function App (props) { 
-    return (
-      <Router>
-        <NavigationBar/>
-        <Routes>         
-          <Route exact path="/about" element={<AboutMe  />}/>
-          <Route exact path="/education" element={<Education  />}/>
-          <Route exact path="/experience" element={<Experience  />}/>
-          <Route exact path="/projects" element={<Projects  />}/>
-          <Route exact path="*" element={<AboutMe  />}/>
-         </Routes>
-      </Router>
-    );
+function App(props) {
+  return (
+    <div>
+      <NavigationBar />
+      <AboutMe />
+      <Education />
+      <Experience />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
