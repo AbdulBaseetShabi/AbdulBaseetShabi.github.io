8dc6407d2642a6da0270fc3760f50cfaa4e30479
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-20 07:24:49 -0400

    single page application

--- src/widget/navigationbar/navigationbar.js
+++ src/widget/navigationbar/navigationbar.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./navigationbar.css";
 
 const ROUTES = [
-  { name: "about me", route: "/about" },
-  { name: "education", route: "/education" },
-  { name: "experiences", route: "/experience" },
-  { name: "projects", route: "/projects" },
+  { name: "about me", route: "about" },
+  { name: "education", route: "education" },
+  { name: "experiences", route: "experience" },
+  { name: "projects", route: "projects" },
 ];
 
 const LINK_STYLE = {
@@ -35,9 +34,9 @@ class NavigationBar extends React.Component {
         <div id="nav-bar-content">
           {ROUTES.map((route, index) => {
             return (
-              <Link to={route.route} className="nav-bar-link" key={index} style={LINK_STYLE}>
+              <a href={`#${route.route}`} className="nav-bar-link" key={index} style={LINK_STYLE}>
                 <label style={{lineHeight: '40px'}}>{route.name}</label>
-              </Link>
+              </a>
             );
           })}
         </div>
