61d83d46a07bb3a521ef1f35caa19868c2ed10b0
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-21 07:59:34 -0400

    loading page while getting data from API

--- src/widget/loadingpage/loadingpage.js
+++ src/widget/loadingpage/loadingpage.js
@@ -1,7 +1,37 @@
 import "./loadingpage.css";
 
-function Loading(){
-
+function LoadingPage() {
+  return (
+    <div
+      className="page"
+      style={{ backgroundColor: "black", color: "white", display: "flex" }}
+    >
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <label className="inline-block-label" style={{ fontSize: "1.5rem" }}>
+            Loading
+          </label>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Loading;
+export default LoadingPage;
