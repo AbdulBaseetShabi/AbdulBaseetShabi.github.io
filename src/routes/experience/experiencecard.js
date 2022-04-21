31114bfc5002944624c95de6b9a84f629704b3dd
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-21 01:12:38 -0400

    Added languages used as part of experience card

--- src/routes/experience/experiencecard.js
+++ src/routes/experience/experiencecard.js
@@ -9,40 +9,21 @@ function ExperienceCard(props) {
   let exeprience = props.experience;
   return (
     <div className="experience-card" style={{borderLeft: `5px solid ${CARD_COLOR[props.type]}`}}>
-      <label className="inline-block-label">{exeprience.title} </label>
-      <label className="inline-block-label"> {exeprience.company} </label>
+      <label className="inline-block-label" style={{fontWeight: 'bolder'}}>{exeprience.title} </label>
+      <label className="inline-block-label" style={{fontWeight: 'bold'}}> {exeprience.company} </label>
       <label className="inline-block-label">{exeprience.location}</label>
       <label className="inline-block-label">{exeprience.date_from} - {exeprience.date_to}</label>
+      {props.type !== 'part' ? <label>{
+        exeprience.languages_and_frameworks.map((x, i)=>{
+          if (i === (exeprience.languages_and_frameworks.length - 1)){
+            return (` ${x} `)
+          }
+
+          return (` ${x} |`)
+        })
+      }</label>: null}
     </div>
   );
 }
 
 export default ExperienceCard;
-
-// {
-//   company: "The Incubator - The Co-operators Group Limited",
-//   date_from: "May 2019",
-//   date_to: "Aug. 2019",
-//   title: "Software Developer",
-//   location: "Waterloo, ON, Canada",
-//   tools_and_technologies: ["Git", "Atlassian", "Visual Studio", "MongoDB"],
-//   languages_and_frameworks: [
-//     "HTML",
-//     "CSS",
-//     "JavaScript",
-//     "Angular",
-//     "NodeJS",
-//   ],
-//   summary: [],
-//   isCoop: true,
-// },
-// {
-//   company: "Mathematics and Statistics Support - WLU",
-//   date_from: "Jan. 2019",
-//   date_to: "April 2021",
-//   title: "Peer Tutor/Mentor",
-//   location: "Waterloo, ON, Canada",
-//   main_skills: ["Communication", "Punctuality", "Knowledgeable", "Patience"],
-//   summary: [],
-//   isPartTime: true,
-// },
