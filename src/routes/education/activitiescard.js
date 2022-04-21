7134067f70c99b610aa86fcf3aeae7f1001e8d14
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-04-21 07:58:50 -0400

    each value on its own line

--- src/routes/education/activitiescard.js
+++ src/routes/education/activitiescard.js
@@ -1,9 +1,9 @@
 function ActivitiesCard(props){
   let activity = props.activity;
   return (
-    <div className="activity-card" style={{minWidth: props.width, height: '50px'}} onClick={()=>props.showReflection(props.index)}>
-      <label className="inline-block-label"> {activity.title} | {activity.company} </label>
-      <br/>
+    <div className="activity-card" style={{width: props.width, height: '80px'}} onClick={()=>props.showReflection(props.index)}>
+      <label className="inline-block-label"> {activity.title}</label>
+      <label className="inline-block-label">{activity.company}</label>
       <label className="inline-block-label" style={{fontStyle: "italic"}}>{activity.date_from} - {activity.date_to}</label>
     </div>
   );
